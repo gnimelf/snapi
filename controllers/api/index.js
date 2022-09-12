a828206704ccc87f4334161f8771a6883a01b9f5
justin <fezzygo@gmail.com>
2022-09-11 22:12:24 -0400

    added thoughts routes to index (controllers/index.js:5,9)

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router()
 
 const userRoutes = require('./users')
 const friendsRoutes = require('./friends')
+const thoughtsRoutes = require('./thoughts')
 
 router.use('/users', userRoutes)
 router.use('/friends', friendsRoutes)
+router.use('/thoughts', thoughtsRoutes)
 
 module.exports = router
