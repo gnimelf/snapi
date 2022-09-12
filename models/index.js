094e85346a9b62acce63230d7c00a3597365dba8
justin <fezzygo@gmail.com>
2022-09-11 22:10:23 -0400

    setup index for models (models/index.js:1-8)

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Friend = require('./Friend')
+const Thought = require('./Thought')
+const Reaction = require('./Reaction')
+
+
+models.exports = {User, Friend, Thought, Reaction}
