3aae3c92b536997482ccf9b784a4316393ff0b11
justin <fezzygo@gmail.com>
2022-09-17 23:54:43 -0400

    added more properties to thoughtSchema (models/Thought.js:2,7-13,17,18)

--- models/Thought.js
+++ models/Thought.js
@@ -1,18 +1,23 @@
 const { Schema, model} = require('mongoose')
+const reactionSchema = require('./Reaction')
 
 const thoughtSchema = new Schema({
     thoughtText: {
         type: String,
-        required: true
+        required: true,
+        min: 1,
+        max: 280
+    },
+    createdAt:{
+        type: Date, 
+        default: Date.now
     },
     username: {
         type: Schema.Types.ObjectId,
-        ref: 'user'
+        ref: 'user',
+        required: true
     },
-    reactions: [{
-        type: Schema.Types.ObjectId,
-        ref: 'reaction'
-    }],
+    reactions: [reactionSchema],
 })
 
 const Thought = model('thought', thoughtSchema)
