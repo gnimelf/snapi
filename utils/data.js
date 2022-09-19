a7fdf0eb9bcfe4d728a11c9ad8638967987dfefc
justin <fezzygo@gmail.com>
2022-09-18 23:13:18 -0400

    removed the adding of friends in the seed data (utils/data.js:8-25,30)

--- utils/data.js
+++ utils/data.js
@@ -5,29 +5,10 @@ const usersList = [
     { username: "anisa", email: "anisa@gmail.com", friends: [] },
 ]
 
-const friendsList = [
-    { thoughtText: "hello from justin", username: "justin" },
-    { thoughtText: "hello from phil", username: "phil" },
-    { thoughtText: "hello from taja", username: "taja" },
-    { thoughtText: "hello from anisa", username: "anisa" },
-]
-
-// Create a friends list
-const getFriends = (currentUser) => {
-    let results = [];
-    for (let i = 0; i < 3; i++) {
-        if (currentUser != friendsList[i].username){
-            results.push(friendsList[i]); 
-        }
-    }
-    return results;
-};
-
 // Create a users list
 const getUsers = () => {
     let users = []
     for (let i = 0; i < usersList.length; i++) {
-        usersList[i].friends = getFriends(usersList[i].username)
         users.push(usersList[i])
     }
     return users
