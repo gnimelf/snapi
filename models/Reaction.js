444862d3ee8606e892f909a5cbfd17a3506dd546
justin <fezzygo@gmail.com>
2022-09-18 23:10:56 -0400

    removed model from  reactionSchema (models/Reaction.js:1,14)

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require('mongoose')
+const { Schema } = require('mongoose')
 
 const reactionSchema = new Schema({
     reactionBody: {
@@ -11,6 +11,4 @@ const reactionSchema = new Schema({
     },
 })
 
-const Reaction = model('reaction', reactionSchema)
-
-module.exports = Reaction
+module.exports = reactionSchema
